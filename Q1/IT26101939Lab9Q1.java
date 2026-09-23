ecc1d4072faef381f72714f01de2ff6087178d59
IT26101939 <it26101939@my.sliit.lk>
2026-09-23 09:56:04 +0530

    Add files via upload

---
+++ Q1/IT26101939Lab9Q1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IT26101939Lab09Q1 {
+    public static void main(String[] args) {
+	 Scanner input = new Scanner(System.in);
+
+        
+     	System.out.print("Enter a: ");
+        double a = input.nextDouble();
+		
+		System.out.print("Enter b: ");
+        double b = input.nextDouble();
+
+        System.out.print("Enter c: ");
+        double c = input.nextDouble();
+		
+		
+        double x = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+
+        
+        System.out.println("x = " + x);
+
+        input.close();
+    }
+
+}
+		
+		
+	
+	
+		
+
